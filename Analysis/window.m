f609b4663a3746c61af9a77fdd1210db2a84a336
Madeleine Futterman <mfutterman@berkeley.edu>
2018-08-01 16:50:35 -0700

    fix missing parentheses in test_fitlm.m

--- Analysis/window.m
+++ Analysis/window.m
@@ -1,5 +1,6 @@
-function  window (hrstart, minstart,timeLength)
-tstart = datetime (2017, 07, 27, hrstart, minstart, 00);
+function  [tstart, tend] = window (hrstart, minstart,secstart, timeLength)
+tstart = datetime (2017, 07, 27, hrstart, minstart, secstart);
 tend = tstart + timeLength;
 xlim ([tstart tend])
-en
+end
+
