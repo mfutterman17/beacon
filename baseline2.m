fbad25d0e9fe50f5a4be80ac0be4bd4da9561942
Madeleine Futterman <mfutterman@berkeley.edu>
2018-08-02 13:53:44 -0700

    baseline2.m: add condition to skip mins that are 1 apart, fix index for fitlm loop

--- baseline2.m
+++ baseline2.m
@@ -90,35 +90,36 @@ RawData.COmin = standardizeMissing(RawData.COmin, [NaN -999 0]);
 COminIndex = find (~ismissing (RawData.COmin));
 PlumeIndex = [1:height(RawData)]';
 PlumeIndex (ismissing(RawData.COPeaks)) = [];
-PlumeIndex = PlumeIndex([1:qty]);
 
 %find plumes where rsquared > 0.6 and slope > 0
-for A = 1:(qty)
+for A = 1:(length(COminIndex)-1)
     Istart = COminIndex(A);
     Iend = COminIndex(A + 1);
-    mdl = fitlm (RawData.deltaCO2([Istart:Iend]), RawData.deltaCO([Istart:Iend]));
-    if mdl.Rsquared.Adjusted > 0.6
-        X = PlumeIndex(Istart < PlumeIndex);
-        indices(:,1) = X(X < Iend);
-        if length(indices) > 1
-            indices(:,2) = RawData.deltaCO(indices);
-            index = (indices(find(indices(:,2) == (max(indices(:,2)))), 1));
-            RawData.PlumeCO(index) = RawData.COAvg(index);
-            RawData.PlumeMin(Istart) = RawData.COAvg(Istart);
-            RawData.PlumeMin(Iend) = RawData.COAvg(Iend);
-        end
-        if length(indices) == 1 
-           line = polyfit (RawData.deltaCO2([Istart:Iend]), RawData.deltaCO([Istart:Iend]),1);
-            slopeDelt = line(1,1);
-            if slopeDelt > 0
-            RawData.PlumeCO(indices) = RawData.COAvg(indices);
-            RawData.PlumeMin(Istart) = RawData.COAvg(Istart);
-            RawData.PlumeMin(Iend) = RawData.COAvg(Iend);
-            RawData.EF(indices) = slopeDelt;
+    if Iend ~= Istart + 1
+        mdl = fitlm (RawData.deltaCO2([Istart:Iend]), RawData.deltaCO([Istart:Iend]));
+        if mdl.Rsquared.Adjusted > 0.6
+            X = PlumeIndex(Istart < PlumeIndex);
+            indices(:,1) = X(X < Iend);
+            if length(indices) > 1
+                indices(:,2) = RawData.deltaCO(indices);
+                index = (indices(find(indices(:,2) == (max(indices(:,2)))), 1));
+                RawData.PlumeCO(index) = RawData.COAvg(index);
+                RawData.PlumeMin(Istart) = RawData.COAvg(Istart);
+                RawData.PlumeMin(Iend) = RawData.COAvg(Iend);
+            end
+            if length(indices) == 1
+                line = polyfit (RawData.deltaCO2([Istart:Iend]), RawData.deltaCO([Istart:Iend]),1);
+                slopeDelt = line(1,1);
+                if slopeDelt > 0
+                    RawData.PlumeCO(indices) = RawData.COAvg(indices);
+                    RawData.PlumeMin(Istart) = RawData.COAvg(Istart);
+                    RawData.PlumeMin(Iend) = RawData.COAvg(Iend);
+                    RawData.EF(indices) = slopeDelt;
+                end
             end
         end
     end
-    clearvars indices index 
+    clearvars indices index
 end
 RawData.PlumeMin = standardizeMissing(RawData.PlumeMin, [0 NaN]);
 RawData.PlumeCO = standardizeMissing(RawData.PlumeCO, [0 NaN]);
