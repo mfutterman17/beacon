4c690b027cdbbe008c8a996bdd6ab1081b6bb005
Madeleine Futterman <mfutterman@berkeley.edu>
2018-08-01 17:11:53 -0700

    fix output.Width500 to match variable RawData in test_fitlm.m

--- Analysis/test_fitlm.m
+++ Analysis/test_fitlm.m
@@ -6,26 +6,26 @@ COminIndex = find (~ismissing (RawData.COmin));
 for A = 1:(length)
     Istart = COminIndex(A);
     Iend = COminIndex(A + 1);
-    mdl = fitlm (output.Width500.deltaCO2([Istart:Iend]), output.Width500.deltaCO([Istart:Iend]));
+    mdl = fitlm (RawData.deltaCO2([Istart:Iend]), RawData.deltaCO([Istart:Iend]));
     if mdl.Rsquared.Adjusted > 0.6
         X = PlumeIndex(Istart < PlumeIndex);
         indices(:,1) = X(X < Iend);
         if length(indices) > 1
-            indices(:,2) = output.Width500.deltaCO(indices);
+            indices(:,2) = RawData.deltaCO(indices);
             index = (indices(find(indices(:,2) == (max(indices(:,2)))), 1));
-            output.Width500.PlumeCO(index) = output.Width500.COAvg(index);
-            output.Width500.PlumeMin(Istart) = output.Width500.COAvg(Istart);
-            output.Width500.PlumeMin(Iend) = output.Width500.COAvg(Iend);
+            RawData.PlumeCO(index) = RawData.COAvg(index);
+            RawData.PlumeMin(Istart) = RawData.COAvg(Istart);
+            RawData.PlumeMin(Iend) = RawData.COAvg(Iend);
         end
         if length(indices) == 1
-            output.Width500.PlumeCO(indices) = output.Width500.COAvg(indices);
-            output.Width500.PlumeMin(Istart) = output.Width500.COAvg(Istart);
-            output.Width500.PlumeMin(Iend) = output.Width500.COAvg(Iend);
+            RawData.PlumeCO(indices) = RawData.COAvg(indices);
+            RawData.PlumeMin(Istart) = RawData.COAvg(Istart);
+            RawData.PlumeMin(Iend) = RawData.COAvg(Iend);
         end
     end
     clearvars indices index
 end
-output.Width500.PlumeMin = standardizeMissing(output.Width500.PlumeMin, [0 NaN]);
-output.Width500.PlumeCO = standardizeMissing(output.Width500.PlumeCO, [0 NaN]);
+RawData.PlumeMin = standardizeMissing(RawData.PlumeMin, [0 NaN]);
+RawData.PlumeCO = standardizeMissing(RawData.PlumeCO, [0 NaN]);
 end
 
