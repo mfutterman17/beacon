9eaf87ba8d58b3545d13cbc9de5899bcb4aee368
Madeleine Futterman <mfutterman@berkeley.edu>
2018-08-03 12:59:49 -0700

    baseline2.m: add peak prominence > 0.025 condition

--- baseline2.m
+++ baseline2.m
@@ -45,20 +45,34 @@ RawData.deltaCO = RawData.COAvg - RawData.baseCO;
 RawData.deltaCO2 = RawData.CO2Avg - RawData.baseCO2;
 
 % find CO peaks
-COPeaks = islocalmax(RawData.deltaCO,'MinSeparation',minutes(3),'SamplePoints',RawData.time);
+[COPeaks, prominence] = islocalmax(RawData.deltaCO,'MinSeparation',minutes(3),'SamplePoints',RawData.time);
+% identify peaks w/ promincence < 0.025 
+tooLow = find (prominence < 0.025);
+
+% establish CO peaks
 RawData.COPeaks = RawData.deltaCO;
 RawData.COPeaks (~COPeaks) = NaN;
+RawData.COPeaks(tooLow) = NaN;
+
 RawData.PeakCOAvg = RawData.COAvg;
 RawData.PeakCOAvg (~COPeaks) = NaN;
+RawData.PeakCOAvg(tooLow) = NaN;
 
 fprintf 'CO peaks calculated. '
 
 % find CO2 peaks
-CO2Peaks = islocalmax(RawData.deltaCO2,'MinSeparation',minutes(3),'SamplePoints',RawData.time);
+[CO2Peaks, prominence] = islocalmax(RawData.deltaCO2,'MinSeparation',minutes(3),'SamplePoints',RawData.time);
+% identify peaks w/ promincence < 0.025 
+tooLow = find (prominence < 0.025);
+
+%establish CO2 peaks
 RawData.CO2Peaks = RawData.deltaCO2;
 RawData.CO2Peaks (~CO2Peaks) = NaN;
+RawData.CO2Peaks(tooLow) = NaN;
+
 RawData.PeakCO2Avg = RawData.CO2Avg;
 RawData.PeakCO2Avg (~CO2Peaks) = NaN;
+RawData.PeakCO2Avg(tooLow) = NaN;
 
 fprintf 'CO2 peaks calculated. '
 
