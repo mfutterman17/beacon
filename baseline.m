141fef93e874e02e51d0a291ce4b352a732c1b58
Madeleine Futterman <mfutterman@berkeley.edu>
2018-08-07 19:00:22 -0700

    baseline.m: peaks every 2 min, match CO w/CO2, calc EF and Rsq & baseline2.m: same as baseline.m but peaks every 3 min

--- baseline.m
+++ baseline.m
@@ -1,4 +1,4 @@
-function RawData = baseline (RawData, WindowWidth)
+function RawData = baseline2 (RawData, WindowWidth)
 % Calculate CO baseline
 COAvg = RawData.COAvg;
 CO2Avg = RawData.CO2Avg;
@@ -6,7 +6,7 @@ baseCO = NaN(size(COAvg));
 baseCO2 = NaN (size(CO2Avg));
 halfWidth = floor(WindowWidth/2);
 halfWidthmin1 = halfWidth - 1;
-parfor A = (halfWidth:(height(RawData)-halfWidth));
+parfor A = (halfWidth:(height(RawData)-halfWidth))
     window = (COAvg((A - halfWidthmin1):(A + halfWidthmin1)));
     base = mean(window(window < prctile(window,25)));
     baseCO (A) = base;
@@ -21,21 +21,21 @@ end
 RawData.baseCO = baseCO;
 RawData.baseCO2 = baseCO2;
 
-fprintf 'base complete'
+fprintf 'base complete. '
 
 % For times within 30 minutes of ends
 xxbeginning = [1:halfWidthmin1];
 RawData.baseCO2(xxbeginning) = RawData.baseCO2(halfWidth);
 RawData.baseCO(xxbeginning) = RawData.baseCO(halfWidth);
 
-fprintf 'beginning fill complete'
+fprintf 'beginning fill complete. '
 
 A = (height(RawData)-halfWidthmin1):(height(RawData));
 RawData.baseCO2(A) = RawData.baseCO2(height(RawData)-halfWidth);
 RawData.baseCO(A) = RawData.baseCO(height(RawData)-halfWidth);
 
 
-fprintf 'end fill complete'
+fprintf 'end fill complete. '
 
 % Make delta CO variable
 RawData.deltaCO = RawData.COAvg - RawData.baseCO;
@@ -44,90 +44,130 @@ RawData.deltaCO = RawData.COAvg - RawData.baseCO;
 RawData.deltaCO2 = RawData.CO2Avg - RawData.baseCO2;
 
 % find CO peaks
-COPeaks = islocalmax(RawData.deltaCO,'MinSeparation',minutes(3),'SamplePoints',RawData.time);
+[COPeaks, prominence] = islocalmax(RawData.deltaCO,'MinSeparation',minutes(2),'SamplePoints',RawData.time);
 RawData.COPeaks = RawData.deltaCO;
 RawData.COPeaks (~COPeaks) = NaN;
+RawData.pCO = prominence;
 
-fprintf 'CO peaks calculated'
+fprintf 'CO peaks calculated. '
 
 % find CO2 peaks
-CO2Peaks = islocalmax(RawData.deltaCO2,'MinSeparation',minutes(3),'SamplePoints',RawData.time);
+CO2Peaks = islocalmax(RawData.deltaCO2,'MinSeparation',minutes(2),'SamplePoints',RawData.time);
 RawData.CO2Peaks = RawData.deltaCO2;
 RawData.CO2Peaks (~CO2Peaks) = NaN;
 
-fprintf 'CO2 peaks calculated'
+fprintf 'CO2 peaks calculated. '
 
 % find all CO2 values within 30 seconds of CO peaks
-    % find peak times
-        RawData.CO2PeakTimes = RawData.time;
-        RawData.CO2PeakTimes (~CO2Peaks) = NaT;
-    % find indices of CO peaks
-        PeakIndex = [1:height(RawData)]';
-        PeakIndex (~COPeaks) = [];
-    % find indices of CO2 peaks
-        PeakIndexCO2 = [1:height(RawData)]';
-        PeakIndexCO2 (~CO2Peaks) = [];
-    % Make an array for time window centers
-        timeWindow = RawData.time(PeakIndex);
-    % Find "plumes"- CO peaks with CO2 peaks within 30 sec
-        for index = 1:length(PeakIndex)
-            A = PeakIndex(index);
-            B = timeWindow (index);
-            indexbefore = find (RawData.CO2PeakTimes > (B) - seconds(30));
-            timesbefore = RawData.CO2PeakTimes(find (RawData.CO2PeakTimes > ((B) - seconds(30))));
-            valid = any(timesbefore < (B) + seconds (30));
-            if valid == 1;
-                RawData.PlumeCOdelta(A) = RawData.COPeaks(A);
-                RawData.PlumeCOAvg(A) = RawData.COAvg(A);
-                possible = RawData.CO2PeakTimes(indexbefore);
-                CO2PlumeIndex = find(RawData.time == possible(1));
-                RawData.PlumeCO2delta(CO2PlumeIndex) = RawData.CO2Peaks(CO2PlumeIndex);
-                RawData.PlumeCO2Avg(CO2PlumeIndex) = RawData.CO2Avg(CO2PlumeIndex);
-            end
-            if A == (length(PeakIndex)/2)
-                fprintf 'plumes halfway complete'
-            end
-        end
-        RawData.PlumeCO2delta = standardizeMissing (RawData.PlumeCO2delta , [0 NaN]);
-        RawData.PlumeCOdelta = standardizeMissing (RawData.PlumeCOdelta , [0 NaN]);
-        RawData.PlumeCOAvg = standardizeMissing (RawData.PlumeCOAvg, [0 NaN]);
-        RawData.PlumeCO2Avg = standardizeMissing (RawData.PlumeCO2Avg, [0 NaN]);
+% find peak times
+RawData.CO2PeakTimes = RawData.time;
+RawData.CO2PeakTimes (~CO2Peaks) = NaT;
+% find indices of CO peaks
+PeakIndex = [1:height(RawData)]';
+PeakIndex (~COPeaks) = [];
+
+% find indices of CO2 peaks
+PeakIndexCO2 = [1:height(RawData)]';
+PeakIndexCO2 (~CO2Peaks) = [];
+
+% Make an array for time window centers
+timeWindow = RawData.time(PeakIndex);
+
+% Find "plumes"- CO peaks with CO2 peaks within 30 sec
+for index = 1:length(PeakIndex)
+    A = PeakIndex(index);
+    B = timeWindow (index);
+    indexbefore = find (RawData.CO2PeakTimes > (B) - seconds(30));
+    timesbefore = RawData.CO2PeakTimes(find (RawData.CO2PeakTimes > ((B) - seconds(30))));
+    valid = any(timesbefore < (B) + seconds (30));
+    if valid == 1;
+        RawData.match_COdelta(A) = RawData.COPeaks(A);
+        RawData.match_COAvg(A) = RawData.COAvg(A);
+        possible = RawData.CO2PeakTimes(indexbefore);
+        CO2PlumeIndex = find(RawData.time == possible(1));
+        RawData.match_CO2delta(CO2PlumeIndex) = RawData.CO2Peaks(CO2PlumeIndex);
+        RawData.match_CO2Avg(CO2PlumeIndex) = RawData.CO2Avg(CO2PlumeIndex);
+    end
+    if A == (length(PeakIndex)/2)
+        fprintf 'Plumes halfway complete. '
+    end
+end
+RawData.match_CO2delta = standardizeMissing (RawData.match_CO2delta , [0 NaN]);
+RawData.match_COdelta = standardizeMissing (RawData.match_COdelta , [0 NaN]);
+RawData.match_COAvg = standardizeMissing (RawData.match_COAvg, [0 NaN]);
+RawData.match_CO2Avg = standardizeMissing (RawData.match_CO2Avg, [0 NaN]);
+
+fprintf 'Plumes calculated. '
 
 % find indices of CO plume peaks
-    PlumeIndex = [1:height(RawData)]'; 
-    PlumeIndex (ismissing(RawData.PlumeCOdelta)) = [];
+PlumeIndex = [1:height(RawData)]';
+PlumeIndex (ismissing(RawData.match_COdelta)) = [];
+PlumeTimes = RawData.time (PlumeIndex);
+RawData.COminTimes = NaT ([height(RawData),1]);
 
-%set an index window to look for the minima
-    for B = 1:(length(PlumeIndex)-1)
-        COmin = min((RawData.deltaCO((PlumeIndex(B)):(PlumeIndex(B+1)))),[] ,'omitnan');
-        % find possible indices of COmin in COAvg
-        COminIndex = find ((RawData.deltaCO == COmin ));
-            % select the index between plumes
-            for X = (1:length(COminIndex)) 
-                if COminIndex(X) > (PlumeIndex(B)) && COminIndex(X) < (PlumeIndex(B+1));
-                   I = COminIndex(X);
-                end
-        % If greater than a minute, record it as a minimum, else dont record it as a minimum
-           if ((abs( (RawData.time (I)) - (RawData.time(PlumeIndex(B))))) > minutes(1)) && ((abs((RawData.time (PlumeIndex(B+1)))- (RawData.time (I)) ))> minutes(1));
-              RawData.COmin (I) = COmin;
-              RawData.COminTimes (I) = RawData.time (I);
-           end
-         end 
-      end 
- %% convert empty rows in COmin to NaN
-    RawData.COmin = standardizeMissing(RawData.COmin, [NaN -999 0]);
+
+% find minima btwn peaks
+for B = 1:(length(PlumeIndex)-1)
+    Istart = PlumeIndex (B);
+    Iend = PlumeIndex(B+1);
+    COminPoss = islocalmin (RawData.deltaCO([Istart:Iend]));
+    COmin = RawData ([Istart:Iend], {'deltaCO', 'time'});
+    COmin = COmin(COminPoss,:);
+    
+    % find minimum closest, but >= 1 minute away from low peak
+    for A = 1:(height(COmin))
+        if COmin.time (A) >= ( PlumeTimes (B) + minutes(1) )
+            index = find (RawData.time == COmin.time(A));
+            RawData.COminTimes (index) = COmin.time (A);
+            if ~isnat (RawData.COminTimes (index))
+                break
+            end
+        end
+    end
+    clearvars index
+    % find minimum closest, but >= 1 minute away from high peak
+    for C = height(COmin):-1:1
+        if COmin.time (C) <= ( PlumeTimes (B+1) - minutes(1) )
+            index = find (RawData.time == COmin.time(C));
+            RawData.COminTimes (index) = COmin.time (C);
+            if ~isnat (RawData.COminTimes (index))
+                break
+            end
+        end
+    end
+end
+
+clearvars index
+
 % find inidicies of minimum values
-minIndices = find (~ismissing(RawData.COmin));
+minIndices = find (~ismissing(RawData.COminTimes));
+
 % find COAvg values at indices
 for A = 1:length(minIndices)
-    RawData.COminAvgVal (minIndices(A)) = RawData.COAvg (minIndices(A));
+    RawData.COmin (minIndices(A)) = RawData.COAvg (minIndices(A));
 end
-RawData.COminAvgVal = standardizeMissing (RawData.COminAvgVal, [0]);
+RawData.COmin = standardizeMissing (RawData.COmin, [0]);
 
 %find inidicies of minimum values
-minIndices = find (~ismissing(RawData.COmin));
-
-
-
+for A = 1:(length(minIndices)-1)
+    Istart = minIndices(A);
+    Iend = minIndices(A + 1);
+    mdl = fitlm (RawData.deltaCO2([Istart:Iend]), RawData.deltaCO([Istart:Iend]));
+    index = PlumeIndex(Istart < PlumeIndex);
+    index = index(index < Iend);
+    RawData.rsquare (index) = mdl.Rsquared.Adjusted;
+    line = polyfit (RawData.deltaCO2([Istart:Iend]), RawData.deltaCO([Istart:Iend]),1);
+    slopeDelt = line(1,1);
+    if slopeDelt > 0
+       RawData.PlumeCO(index) = RawData.COAvg(index);
+       RawData.PlumeMin(Istart) = RawData.COAvg(Istart);
+       RawData.PlumeMin(Iend) = RawData.COAvg(Iend);
+       RawData.EF (index) = slopeDelt;
+    end
+end
+RawData.PlumeMin = standardizeMissing(RawData.PlumeMin, [0 NaN]);
+RawData.PlumeCO = standardizeMissing(RawData.PlumeCO, [0 NaN]);
+RawData.EF = standardizeMissing(RawData.EF, [0 NaN]);
+RawData.rsquare = standardizeMissing (RawData.rsquare, [0 NaN]);
 end
 
--- baseline2.m
+++ baseline2.m
@@ -1,5 +1,4 @@
 function RawData = baseline2 (RawData, WindowWidth)
-%this is a comment
 % Calculate CO baseline
 COAvg = RawData.COAvg;
 CO2Avg = RawData.CO2Avg;
@@ -46,103 +45,129 @@ RawData.deltaCO2 = RawData.CO2Avg - RawData.baseCO2;
 
 % find CO peaks
 [COPeaks, prominence] = islocalmax(RawData.deltaCO,'MinSeparation',minutes(3),'SamplePoints',RawData.time);
-% identify peaks w/ promincence < 0.008 
-tooLow = find (prominence < 0.008);
-
-% establish CO peaks
 RawData.COPeaks = RawData.deltaCO;
 RawData.COPeaks (~COPeaks) = NaN;
-RawData.COPeaks(tooLow) = NaN;
-
-RawData.PeakCOAvg = RawData.COAvg;
-RawData.PeakCOAvg (~COPeaks) = NaN;
-RawData.PeakCOAvg(tooLow) = NaN;
+RawData.pCO = prominence;
 
 fprintf 'CO peaks calculated. '
 
 % find CO2 peaks
-[CO2Peaks, prominence] = islocalmax(RawData.deltaCO2,'MinSeparation',minutes(3),'SamplePoints',RawData.time);
-% identify peaks w/ promincence < 0.008 
-tooLow = find (prominence < 0.008);
-
-%establish CO2 peaks
+CO2Peaks = islocalmax(RawData.deltaCO2,'MinSeparation',minutes(3),'SamplePoints',RawData.time);
 RawData.CO2Peaks = RawData.deltaCO2;
 RawData.CO2Peaks (~CO2Peaks) = NaN;
-RawData.CO2Peaks(tooLow) = NaN;
-
-RawData.PeakCO2Avg = RawData.CO2Avg;
-RawData.PeakCO2Avg (~CO2Peaks) = NaN;
-RawData.PeakCO2Avg(tooLow) = NaN;
 
 fprintf 'CO2 peaks calculated. '
 
-
+% find all CO2 values within 30 seconds of CO peaks
+% find peak times
+RawData.CO2PeakTimes = RawData.time;
+RawData.CO2PeakTimes (~CO2Peaks) = NaT;
 % find indices of CO peaks
-PlumeIndex = [1:height(RawData)]';
-PlumeIndex (ismissing(RawData.COPeaks)) = [];
+PeakIndex = [1:height(RawData)]';
+PeakIndex (~COPeaks) = [];
 
-%set an index window to look for the minima
+% find indices of CO2 peaks
+PeakIndexCO2 = [1:height(RawData)]';
+PeakIndexCO2 (~CO2Peaks) = [];
+
+% Make an array for time window centers
+timeWindow = RawData.time(PeakIndex);
+
+% Find "plumes"- CO peaks with CO2 peaks within 30 sec
+for index = 1:length(PeakIndex)
+    A = PeakIndex(index);
+    B = timeWindow (index);
+    indexbefore = find (RawData.CO2PeakTimes > (B) - seconds(30));
+    timesbefore = RawData.CO2PeakTimes(find (RawData.CO2PeakTimes > ((B) - seconds(30))));
+    valid = any(timesbefore < (B) + seconds (30));
+    if valid == 1;
+        RawData.match_COdelta(A) = RawData.COPeaks(A);
+        RawData.match_COAvg(A) = RawData.COAvg(A);
+        possible = RawData.CO2PeakTimes(indexbefore);
+        CO2PlumeIndex = find(RawData.time == possible(1));
+        RawData.match_CO2delta(CO2PlumeIndex) = RawData.CO2Peaks(CO2PlumeIndex);
+        RawData.match_CO2Avg(CO2PlumeIndex) = RawData.CO2Avg(CO2PlumeIndex);
+    end
+    if A == (length(PeakIndex)/2)
+        fprintf 'Plumes halfway complete. '
+    end
+end
+RawData.match_CO2delta = standardizeMissing (RawData.match_CO2delta , [0 NaN]);
+RawData.match_COdelta = standardizeMissing (RawData.match_COdelta , [0 NaN]);
+RawData.match_COAvg = standardizeMissing (RawData.match_COAvg, [0 NaN]);
+RawData.match_CO2Avg = standardizeMissing (RawData.match_CO2Avg, [0 NaN]);
+
+fprintf 'Plumes calculated. '
+
+% find indices of CO plume peaks
+PlumeIndex = [1:height(RawData)]';
+PlumeIndex (ismissing(RawData.match_COdelta)) = [];
+PlumeTimes = RawData.time (PlumeIndex);
+RawData.COminTimes = NaT ([height(RawData),1]);
+
+
+% find minima btwn peaks
 for B = 1:(length(PlumeIndex)-1)
-    COmin = min((RawData.deltaCO((PlumeIndex(B)):(PlumeIndex(B+1)))),[] ,'omitnan');
-    % find possible indices of COmin in COAvg
-    COminIndex = find ((RawData.deltaCO == COmin ));
-    % select the index between plumes
-    for X = (1:length(COminIndex))
-        if COminIndex(X) > (PlumeIndex(B)) && COminIndex(X) < (PlumeIndex(B+1));
-            I = COminIndex(X);
+    Istart = PlumeIndex (B);
+    Iend = PlumeIndex(B+1);
+    COminPoss = islocalmin (RawData.deltaCO([Istart:Iend]));
+    COmin = RawData ([Istart:Iend], {'deltaCO', 'time'});
+    COmin = COmin(COminPoss,:);
+    
+    % find minimum closest, but >= 1 minute away from low peak
+    for A = 1:(height(COmin))
+        if COmin.time (A) >= ( PlumeTimes (B) + minutes(1) )
+            index = find (RawData.time == COmin.time(A));
+            RawData.COminTimes (index) = COmin.time (A);
+            if ~isnat (RawData.COminTimes (index))
+                break
+            end
         end
-        % If greater than a minute, record it as a minimum, else dont record it as a minimum
-        if ((abs( (RawData.time (I)) - (RawData.time(PlumeIndex(B))))) > minutes(1)) && ((abs((RawData.time (PlumeIndex(B+1)))- (RawData.time (I)) ))> minutes(1));
-            RawData.COmin (I) = COmin;
-            RawData.COminTimes (I) = RawData.time (I);
+    end
+    clearvars index
+    % find minimum closest, but >= 1 minute away from high peak
+    for C = height(COmin):-1:1
+        if COmin.time (C) <= ( PlumeTimes (B+1) - minutes(1) )
+            index = find (RawData.time == COmin.time(C));
+            RawData.COminTimes (index) = COmin.time (C);
+            if ~isnat (RawData.COminTimes (index))
+                break
+            end
         end
     end
 end
-% convert empty rows in COmin to NaN
-RawData.COmin = standardizeMissing(RawData.COmin, [NaN -999 0]);
 
-COminIndex = find (~ismissing (RawData.COmin));
-PlumeIndex = [1:height(RawData)]';
-PlumeIndex (ismissing(RawData.COPeaks)) = [];
+clearvars index
 
-%find plumes where rsquared > 0.6 and slope > 0
-for A = 1:(length(COminIndex)-1)
-    Istart = COminIndex(A);
-    Iend = COminIndex(A + 1);
-    if Iend ~= (Istart + 1)
-        mdl = fitlm (RawData.deltaCO2([Istart:Iend]), RawData.deltaCO([Istart:Iend]));
-        if mdl.Rsquared.Adjusted > 0.6
-            X = PlumeIndex(Istart < PlumeIndex);
-            indices(:,1) = X(X < Iend);
-            if length(indices) > 1
-                indices(:,2) = RawData.deltaCO(indices);
-                index = (indices(find(indices(:,2) == (max(indices(:,2)))), 1));
-                line = polyfit (RawData.deltaCO2([Istart:Iend]), RawData.deltaCO([Istart:Iend]),1);
-                slopeDelt = line(1,1);
-                if slopeDelt > 0
-                    RawData.PlumeCO(index) = RawData.COAvg(index);
-                    RawData.PlumeMin(Istart) = RawData.COAvg(Istart);
-                    RawData.PlumeMin(Iend) = RawData.COAvg(Iend);
-                    RawData.EF (index) = slopeDelt;
-                end
-            end
-            if length(indices) == 1
-                line = polyfit (RawData.deltaCO2([Istart:Iend]), RawData.deltaCO([Istart:Iend]),1);
-                slopeDelt = line(1,1);
-                if slopeDelt > 0
-                    RawData.PlumeCO(indices) = RawData.COAvg(indices);
-                    RawData.PlumeMin(Istart) = RawData.COAvg(Istart);
-                    RawData.PlumeMin(Iend) = RawData.COAvg(Iend);
-                    RawData.EF(indices) = slopeDelt;
-                end
-            end
-        end
+% find inidicies of minimum values
+minIndices = find (~ismissing(RawData.COminTimes));
+
+% find COAvg values at indices
+for A = 1:length(minIndices)
+    RawData.COmin (minIndices(A)) = RawData.COAvg (minIndices(A));
+end
+RawData.COmin = standardizeMissing (RawData.COmin, [0]);
+
+%find inidicies of minimum values
+for A = 1:(length(minIndices)-1)
+    Istart = minIndices(A);
+    Iend = minIndices(A + 1);
+    mdl = fitlm (RawData.deltaCO2([Istart:Iend]), RawData.deltaCO([Istart:Iend]));
+    index = PlumeIndex(Istart < PlumeIndex);
+    index = index(index < Iend);
+    RawData.rsquare (index) = mdl.Rsquared.Adjusted;
+    line = polyfit (RawData.deltaCO2([Istart:Iend]), RawData.deltaCO([Istart:Iend]),1);
+    slopeDelt = line(1,1);
+    if slopeDelt > 0
+       RawData.PlumeCO(index) = RawData.COAvg(index);
+       RawData.PlumeMin(Istart) = RawData.COAvg(Istart);
+       RawData.PlumeMin(Iend) = RawData.COAvg(Iend);
+       RawData.EF (index) = slopeDelt;
     end
-    clearvars indices index
 end
 RawData.PlumeMin = standardizeMissing(RawData.PlumeMin, [0 NaN]);
 RawData.PlumeCO = standardizeMissing(RawData.PlumeCO, [0 NaN]);
 RawData.EF = standardizeMissing(RawData.EF, [0 NaN]);
-
+RawData.rsquare = standardizeMissing (RawData.rsquare, [0 NaN]);
 end
 
