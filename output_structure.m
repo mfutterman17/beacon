a01e811a8ebc939c77703a6b67a38878341cf0ff
mfutterman <mfutterman@berkeley.edu>
2018-08-09 11:23:53 -0700

    output_structure: run baseline.m

--- output_structure.m
+++ output_structure.m
@@ -1,7 +1,7 @@
 function [output] = windows_for_baseline(RawData,WindowWidth)
 for iWindow = 1:length(WindowWidth)
  window_fn = sprintf('Width%d', WindowWidth(iWindow));
- output.(window_fn) = baseline2 (RawData, WindowWidth(iWindow));
+ output.(window_fn) = baseline (RawData, WindowWidth(iWindow));
  if iWindow == 1
     output = struct(window_fn, output.(window_fn));
  end
